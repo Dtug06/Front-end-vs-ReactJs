433f344a712d65e52a6e2972efef2ffdbaaf7efe
Dtug06 <tuan20062012@gmail.com>
2025-08-29 00:39:08 +0700

    ss19

---
+++ PTIT_CNTT4_IT202_Session19/PTIT_CNTT4_IT202_Session19/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Bai1 from './components/PTIT_CNTT4_IT202_SESSION19_BAI01/Bai1'
+import Bai3 from './components/PTIT_CNTT4_IT202_SESSION19_BAI03/Bai3'
+import Bai4 from './components/PTIT_CNTT4_IT202_SESSION19_BAI04/Bai4'
+import Bai5 from './components/PTIT_CNTT4_IT202_SESSION19_BAI05/Bai5'
+import Bai6 from './components/PTIT_CNTT4_IT202_SESSION19_BAI06/Bai6'
+import Bai7 from './components/PTIT_CNTT4_IT202_SESSION19_BAI07/Bai7'
+import Bai8 from './components/PTIT_CNTT4_IT202_SESSION19_BAI08/Bai8'
+export default function App() {
+  return (
+    <div>
+     <Bai1 />
+     <Bai3 />
+     <Bai4 />
+     <Bai5 />
+     <Bai6 />
+     <Bai7 />
+     <Bai8 />
+    </div>
+  )
+}
