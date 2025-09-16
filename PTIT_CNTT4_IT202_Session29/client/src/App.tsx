c57a0af2d46a926fadc4033c4f92754d636b6e32
Dtug06 <tuan20062012@gmail.com>
2025-09-16 08:26:28 +0700

    ss29updateted

--- PTIT_CNTT4_IT202_Session29/client/src/App.tsx
+++ PTIT_CNTT4_IT202_Session29/client/src/App.tsx
@@ -1,56 +1,14 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-type User = {
-  id: number;
-  name: string;
-};
-export default function App() {
-  const [users, setUsers] = useState<any>([]);
-  const [name,setName]= useState<string>("");
- // function lấy data
-  async function getData() {
-    let res = await axios.get("http://localhost:3000/users");
-    console.log("res", res);
-    setUsers([...res.data]);
-  }
-  // function thêm user vào database (json-server);
-  async function addNewUser(user:any){
-      let res= await axios.post("http://localhost:3000/users",user);
-  }
-  useEffect(() => {
-    getData();
-  }, []);
+import React from 'react'
 
-  // tạo hàm thêm mới user
-  const addUser=()=>{
-      addNewUser({name:name});
-      setUsers([...users]);
-      setName("");
-  }
-  const handleChange=(e:any)=>{
-      setName(e.target.value);
-  }
-  const deleteUser=(id:number)=>{
-    console.log(id);
-    axios.delete(`http://localhost:3000/users/${id}`);
-    axios.patch(`http://localhost:3000/products/${id}`,{
-      name:"iphone5"
-    });
-    
-  }
+import Bai7 from './components/Bai78'
+export default function App() {
   return (
     <div>
-      học API
-      <h1> danh sách user</h1>
-      <input value={name} onChange={handleChange} type="text" />
-      <button onClick={addUser}>thêm user</button>
-      <ul>
-        {users.map((item: any, index: number) => {
-          return <li key={index}> {item.name}
-           <button onClick={()=>deleteUser(item.id)}>xóa</button>
-          </li>;
-        })}
-      </ul>
+      {/* <Bai12></Bai12> */}
+      {/* <Bai34></Bai34> */}
+      {/* <Bai5></Bai5> */}
+      {/* <Bai6></Bai6> */}
+      <Bai7></Bai7>
     </div>
-  );
+  )
 }
